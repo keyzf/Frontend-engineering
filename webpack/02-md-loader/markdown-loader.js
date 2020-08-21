3f5e820c0446a32f94e6d9faaf47ad8c68249c72
JakePrim <1760473022@qq.com>
2020-08-21 23:16:04 +0800

    loader

---
+++ webpack/02-md-loader/markdown-loader.js
@@ -0,0 +1,14 @@
+const marked = require('marked');
+
+module.exports = source => {
+    // console.log(source);
+    //只能返回一个 javascript代码 / 下一个loader处理 other-loader
+    // return 'console.log("hello ~")'
+    const html = marked(source);
+    // return `module.exports=${JSON.stringify(html)}`
+    // ES Module导出
+    // return `export default ${JSON.stringify(html)}`
+
+    //返回HTML字符串 交给下一个html-loader处理
+    return html
+}
---
+++ webpack/02-md-loader/src/about.md
@@ -0,0 +1,5 @@
+# this is me
+
+This is MD-Loader
+
+## Use
