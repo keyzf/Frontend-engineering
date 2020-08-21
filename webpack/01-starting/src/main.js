57264b801a78834c255c83ecc519688c8c6c35e8
JakePrim <1760473022@qq.com>
2020-08-22 00:09:21 +0800

    webpack

--- webpack/01-starting/src/main.js
+++ webpack/01-starting/src/main.js
@@ -1,6 +1,20 @@
-import createHeading from './heading.js'
+import createHeading from './heading.js'//导入js文件
 import './main.css'//导入css文件
+import icon from './1.jpg';//导入文件资源
+import footerHtml from './footer.html';//导入HTML文件 html字符串 需要使用html-loader
 
 const heading = createHeading();
 
 document.body.appendChild(heading);
+
+const img = new Image();
+img.src=icon;
+img.addEventListener('click',()=>{
+    console.log('click');
+})
+document.body.appendChild(img);
+
+//借助write输出HTML 需要为HTML配置对应的loader否则webpack没有办法认识这个HTML
+document.write(footerHtml);
+
+
--- webpack/01-starting/webpack.config.js
+++ webpack/01-starting/webpack.config.js
@@ -7,17 +7,67 @@ module.exports={
     //指定输出文件的路径-就是编译后的js
     output: {
         filename:'bundle.js',
-        path:path.join(__dirname,'dist')
+        path:path.join(__dirname,'dist'),
+        publicPath:'dist/'//网站的根目录 用于资源目录 否则无法找到资源文件
     },
     //添加loader配置 处理其他类型资源文件
     module:{
         rules:[
+            {
+                test:/.js$/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env']
+                    }
+                }
+            },
             {
                 test:/.css$/,//匹配文件
                 use:[//配置多个loader是从后往前执行 一定要先执行css-loader
                     'style-loader', // 把css-loader编译后的结果 通过style标签的形式追加到界面上
                     'css-loader' //
                 ],//该文件使用的加载器  yarn add style-loader --dev  
+            },
+            // {
+            //     test:/.jpg$/,
+            //     use:['file-loader'] //文件资源加载器
+            // },
+            {
+                //Data url 的方式表示文件
+                test:/.jpg$/,
+                use:{
+                    loader:'url-loader',
+                    options:{
+                        limit:10 * 1024 //限制10KB一下的文件交给url-loader 大于10KB的文件使用file-loader 一定要安装file-loader模块
+                    }
+                }
+            },
+            {
+                test:/\.html$/,
+                use:{
+                    loader:'html-loader',
+                    options:{
+                        // attrs: ['img:src','a:href']//对HTML的属性处理 此处是老版本的写法 由于新版本升级不再是这个写法了
+                        //1.2.1 版本
+                        //如果传递boolean True 值允许处理所有默认元素和属性，false 禁用处理所有属性。
+                        //如果传递object则进行处理
+                        attributes:{
+                            list:[
+                                {
+                                    tag:'img',
+                                    attribute:'src',
+                                    type:'src'
+                                },
+                                {
+                                    tag:'a',//对应的标签名
+                                    attribute:'href',//对应的属性名
+                                    type:'src'//对应的属性类型
+                                }
+                            ]
+                        }
+                    }
+                }
             }
         ]
     }
